f97a9d0d0ddeba2918a725737a5d3ab692fd497d
stetsenkovladyslav <50491481+stetsenkovladyslav@users.noreply.github.com>
2021-01-19 22:21:54 +0200

    HW 14

---
+++ src/main/java/com/example/Cursor_HW14/CursorHw14Application.java
@@ -0,0 +1,15 @@
+package com.example.Cursor_HW14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CursorHw14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorHw14Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Cursor_HW14/SchedulerExample.java
@@ -0,0 +1,16 @@
+package com.example.Cursor_HW14;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class SchedulerExample {
+
+
+    @Scheduled(cron = "*/1 * * * * *") //Format: second, minute, hour, day, month, day of the week
+    public void reportCurrentData() {
+        System.out.println("Scheduler working: " + new Date());
+    }
+}
