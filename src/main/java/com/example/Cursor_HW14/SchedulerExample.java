a7a056d69ad6bd0c6d49ce55b29bb6706f5090a8
stetsenkovladyslav <50491481+stetsenkovladyslav@users.noreply.github.com>
2021-01-19 22:25:11 +0200

    HW 14

--- src/main/java/com/example/Cursor_HW14/SchedulerExample.java
+++ src/main/java/com/example/Cursor_HW14/SchedulerExample.java
@@ -8,7 +8,6 @@ import java.util.Date;
 @Component
 public class SchedulerExample {
 
-
     @Scheduled(cron = "*/1 * * * * *") //Format: second, minute, hour, day, month, day of the week
     public void reportCurrentData() {
         System.out.println("Scheduler working: " + new Date());
